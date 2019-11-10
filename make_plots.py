991548bd46b56ee4443329f727901b1a3a7edda4
Bastian Rieck <bastian@rieck.ru>
2019-11-10 17:43:28 +0100

    Added basic script for creating some plots

---
+++ make_plots.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# A simple script that makes use of Seaborn to create a set of
+# interesting plots to analyse the reviews. Please note that I
+# hard-coded everything for ICLR 2020 for now.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+sns.set_style("dark")
+
+df = pd.read_csv('2020.csv', index_col='review_id')
+df['experience'] = df.experience.astype('category')
+
+plt.figure(figsize=(4, 5))
+
+ax = sns.boxplot(x=df['rating'], y=df['n_words'], data=df)
+
+ax.set_xlabel('Rating')
+ax.set_ylabel('Number of words')
+
+plt.tight_layout()
+plt.savefig('ICLR_2020_boxplots.svg')
+
+plt.clf()
+
+ax = sns.boxplot(x=df['experience'], y=df['n_words'], data=df)
+
+ax.set_xlabel('Experience')
+ax.set_ylabel('Number of words')
+
+plt.tight_layout()
+plt.savefig('ICLR_2020_boxplots_experience.svg')
+
+plt.clf()
+
+g = sns.catplot(
+    x='rating',
+    y='n_words',
+    col='experience',
+    kind='box',
+    data=df,
+    aspect=0.6
+)
+
+g.set_axis_labels('Rating', 'Number of words')
+g.set_titles('Experience = {col_name}')
+
+plt.tight_layout()
+plt.savefig('ICLR_2020_boxplots_categories.svg')
+
+plt.show()
